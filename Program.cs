517d8f2b9d673b430f56344bf67ce65dbadd448f
mohisnchishti <138819824+mohisnchishti@users.noreply.github.com>
2023-07-07 01:04:05 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,39 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace mm
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int number;
-          
-            Console.WriteLine("enter the number");
-            number = Convert.ToInt16(Console.ReadLine());
-             
-            for (int i = 2; i <= number; i++)
-            { 
-                if(IsPrime(i) == true)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-            Console.ReadKey();
-
-        }
-
-        static bool IsPrime(int num)
-        {
-            bool p = true;
-            for(int i = 2; i<num; i++)
+            int a = 0;
+            int b = 1;
+            int num;
+            int c = 0;
+            Console.WriteLine("please enter the number");
+            num=Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("The fabonacci series is");
+            Console.Write(a + " " + b + " ");
+            for (int i = 2; i < num; i++)
             {
-                if(num%i == 0)
-                {
-                    p = false;
-                }
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+
             }
-            return p;
+
+            Console.ReadKey();
         }
     }
 }
