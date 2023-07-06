710415426a9bafdb3f60fd9f643a004a62f6c0c7
mohisnchishti <138819824+mohisnchishti@users.noreply.github.com>
2023-07-07 00:53:51 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,28 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp7
+namespace ConsoleApp6
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n;
-            Console.WriteLine("please enter the size of n");
-            n = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= n; i++)
+            int max, min; 
+            int[] arr = new int[10];
+            Console.WriteLine("enter the numbers");
+            for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 1; j <= i; j++)
-                {
-                    Console.Write("*");
-
-                }
-
-                Console.WriteLine("");
-
-              
+                arr[i] = int.Parse(Console.ReadLine());
             }
+            max= arr[0];
+            min= arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    arr[i] = min;
+                }
+                
+            }
+            Console.WriteLine("minimun number in array is " + min);
+            Console.WriteLine("maximum number in array is " + max);
             Console.ReadKey();
         }
     }
