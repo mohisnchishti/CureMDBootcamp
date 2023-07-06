7b4b0a539afcec56f2f9d4a90e792b2f29ca9cdb
mohisnchishti <138819824+mohisnchishti@users.noreply.github.com>
2023-07-07 00:48:57 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,41 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp10
+namespace ConsoleApp9
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int max, min;
-            int[] arr = new int[10];
-            Console.WriteLine("enter the length of array");
-            int len = int.Parse(Console.ReadLine());
-            Console.WriteLine("enter the numbers of array");
-            for (int i = 0; i < len; i++)
+            string string1;
+          
+            Console.WriteLine("enter the string");
+           string1= Console.ReadLine();
+            for (int i=string1.Length-1; i >= 0; i--)
             {
-                arr[i] = int.Parse(Console.ReadLine());
+                Console.Write(string1[i]);
             }
-            for (int i = 0; i <=len; i++)
-            {
-                for (int j = i+1; j < len; j++)
-                {
-                    if (arr[j] < arr[i])
-                    {
-                        min = arr[j];
-                        arr[j] = arr[i];
-                        arr[i] = min;
-
-                    }
-
-                }
-            }
-            Console.WriteLine(" the numbers of array");
-            for (int i=0; i<len; i++)
-            {
-                Console.WriteLine(arr[i]);
-            }
-            
             Console.ReadKey();
         }
     }
