9742bf59fe75c5b6247e8693ec8206d3f5d542c3
mohisnchishti <138819824+mohisnchishti@users.noreply.github.com>
2023-07-07 08:13:27 +0500

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int[] arr = new int[5];
+            Console.WriteLine("please enter the numbers of array");
+           
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int num;
+            Console.WriteLine("enter the number");
+            num = int.Parse(Console.ReadLine());
+           
+            for (int j = 0; j < 5; j++)
+            {
+                if (num == arr[j])
+                { 
+                Console.WriteLine(num +" is present in th array");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
