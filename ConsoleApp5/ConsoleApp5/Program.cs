49dbf0d5d1926522f8a6671ef685b8d18f8cb338
mohisnchishti <138819824+mohisnchishti@users.noreply.github.com>
2023-07-07 08:15:36 +0500

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1="";
+            string string2;
+            Console.WriteLine("enter string");
+            Console.ReadLine();
+            char[] arr = string1.ToCharArray(); 
+            Array.Reverse(arr);
+            string2 = new string(arr);
+            if (string1 == string2)
+            {
+                Console.WriteLine("string is palindrome");
+
+            }
+            else
+            {
+                Console.WriteLine("string is not palindrome");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max, min; 
+            int[] arr = new int[10];
+            Console.WriteLine("enter the numbers");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            max= arr[0];
+            min= arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    arr[i] = min;
+                }
+                
+            }
+            Console.WriteLine("minimun number in array is " + min);
+            Console.WriteLine("maximum number in array is " + max);
+            Console.ReadKey();
+        }
+    }
+}
